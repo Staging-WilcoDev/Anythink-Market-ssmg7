3d1b5a358361ca58ce57151c00c3070e77d57020
Andrei <andrei.averkin@gmail.com>
2022-09-29 12:21:08 +0000

    format

--- frontend/src/components/Item/CommentInput.js
+++ frontend/src/components/Item/CommentInput.js
@@ -7,30 +7,28 @@ const mapDispatchToProps = (dispatch) => ({
   onSubmit: (payload) => dispatch({ type: ADD_COMMENT, payload }),
 });
 
-
-
 const CommentInput = (props) => {
   // constructor() {
-    // super();
-    // this.state = {
-    //   body: "",
-    // };
+  // super();
+  // this.state = {
+  //   body: "",
+  // };
 
-    // this.setBody = (ev) => {
-    //   this.setState({ body: ev.target.value });
-    // };
+  // this.setBody = (ev) => {
+  //   this.setState({ body: ev.target.value });
+  // };
 
-    // this.createComment = (ev) => {
-    //   ev.preventDefault();
-    //   const payload = agent.Comments.create(this.props.slug, {
-    //     body: this.state.body,
-    //   });
-    //   this.setState({ body: "" });
-    //   this.props.onSubmit(payload);
-    // };
+  // this.createComment = (ev) => {
+  //   ev.preventDefault();
+  //   const payload = agent.Comments.create(this.props.slug, {
+  //     body: this.state.body,
+  //   });
+  //   this.setState({ body: "" });
+  //   this.props.onSubmit(payload);
+  // };
   // }
 
-  const [body, setBody] = useState('');
+  const [body, setBody] = useState("");
 
   const createComment = (ev) => {
     ev.preventDefault();
@@ -41,29 +39,29 @@ const CommentInput = (props) => {
     props.onSubmit(payload);
   };
 
-    return (
-      <form className="card comment-form m-2" onSubmit={createComment}>
-        <div className="card-block">
-          <textarea
-            className="form-control"
-            placeholder="Write a comment..."
-            value={body}
-            onChange={(ev) => setBody(ev.target.value)}
-            rows="3"
-          ></textarea>
-        </div>
-        <div className="card-footer">
-          <img
-            src={props.currentUser.image}
-            className="user-pic mr-2"
-            alt={props.currentUser.username}
-          />
-          <button className="btn btn-sm btn-primary" type="submit">
-            Post Comment
-          </button>
-        </div>
-      </form>
-    );
-}
+  return (
+    <form className="card comment-form m-2" onSubmit={createComment}>
+      <div className="card-block">
+        <textarea
+          className="form-control"
+          placeholder="Write a comment..."
+          value={body}
+          onChange={(ev) => setBody(ev.target.value)}
+          rows="3"
+        ></textarea>
+      </div>
+      <div className="card-footer">
+        <img
+          src={props.currentUser.image}
+          className="user-pic mr-2"
+          alt={props.currentUser.username}
+        />
+        <button className="btn btn-sm btn-primary" type="submit">
+          Post Comment
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default connect(() => ({}), mapDispatchToProps)(CommentInput);
