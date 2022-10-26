91bdc8f27e24f924c02d5eb32db1ae312d3ec5db
Andrei <andrei.averkin@gmail.com>
2022-10-26 20:03:38 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,9 @@ import { Provider } from "react-redux";
 import React from "react";
 import { store, history } from "./store";
 
-
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
+console.log('aa');
 
 import App from "./components/App";
 
